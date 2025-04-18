6edc03ded2cea573205cbcab29943210e8511be1
vetin7 <overgames32661080@gmail.com>
2025-04-17 21:55:03 -0300

    Update atv_03.py

--- atv_03.py
+++ atv_03.py
@@ -1,11 +1,6 @@
-'''Crie uma função que receba uma frase e conte quantos caracteres (sem
-espaço) ela possui.'''
-
-def contagem(frase):
+def carac(frase):
     return len(frase.replace(" ",""))
 
 frase = input("Escreva a sua frase: ")
-
-qtd = contagem(frase)
-
-print(f"Sua frase tem um total de {qtd} caracteres.")
+num = carac(frase)
+print(f"Sua frase tem um total de {num} caracteres.")
