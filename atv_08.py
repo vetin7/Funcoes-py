1711c59a5f2db1d59e9bcff1b7d27db34cc26444
vetin7 <overgames32661080@gmail.com>
2025-04-17 22:00:18 -0300

    Update atv_08.py

--- atv_08.py
+++ atv_08.py
@@ -1,16 +1,10 @@
-'''Crie uma função que receba uma palavra e diga se ela é um palíndromo
-(lê igual de frente para trás).'''
-
 def analise(palavra):
     palavra = palavra.lower().replace(" ", "")
     return palavra[::-1]
 
-palavra = input("Diga a sua palavra: ")
-
+palavra = input("Informe a palavra: ")
 invertido = analise(palavra)
-
 if invertido == palavra:
     print(f"{palavra} é um polindromo.")
-
 else:
     print(f"{palavra} nao é um polindromo.")
