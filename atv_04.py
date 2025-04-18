26ff6aaa9c57717bd9998507dc90db422a01262a
vetin7 <overgames32661080@gmail.com>
2025-04-17 21:56:39 -0300

    Update atv_04.py

--- atv_04.py
+++ atv_04.py
@@ -1,18 +1,14 @@
-'''Crie uma função que receba três notas e retorne a média do aluno.
-○ Mostre se o aluno está aprovado ou reprovado (média mínima 7).'''
-
-def media(n1, n2, n3):
-    media = (n1 + n2 + n3) // 3
+def media(num1, num2, num3):
+    media = (num1 + num2 + num3) // 3
     return media
 
-n1 = int(input("Diga a sua primeira nota: "))
-n2 = int(input("Diga a sua segunda nota: "))
-n3 = int(input("Diga a sua terceira nota: "))
+num1 = int(input("Diga a sua primeira nota: "))
+num2 = int(input("Diga a sua segunda nota: "))
+num3 = int(input("Diga a sua terceira nota: "))
 
-resultado = media(n1, n2, n3)
+resultado = media(num1, num2, num3)
 
 if resultado >= 7:
-    print(f"Voce obteve uma media de {resultado} e foi aprovado")
-
+    print(f"Voce foi aprovado")
 else:
-    print(f"Voce obteve uma media de {resultado} e foi reprovado.")
+    print(f"Voce foi reprovado.")
