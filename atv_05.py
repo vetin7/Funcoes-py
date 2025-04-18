28cc22559867e658cd3ce140ed6bd2d04fb6fe36
vetin7 <overgames32661080@gmail.com>
2025-04-17 21:58:10 -0300

    Update atv_05.py

--- atv_05.py
+++ atv_05.py
@@ -1,10 +1,6 @@
-'''Crie uma função que receba um número e imprima a tabuada desse
-número.'''
-
-def tabuada(n1):
-    print(f"Tabuada do {n1}:")
+def tabuada(num1):
     for i in range(1, 11): 
-        print(f"{n1} x {i} = {n1 * i}")
+        print(f"{num1} x {i} = {num1 * i}")
 
-n1 = int(input("Diga o numero: "))
-tabuada(n1)
+num1 = int(input("Diga o numero: "))
+tabuada(num1)
