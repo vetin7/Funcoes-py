681867c484caec44c49eded78d28ba1c992913e1
vetin7 <overgames32661080@gmail.com>
2025-04-17 21:59:42 -0300

    Update atv_07.py

--- atv_07.py
+++ atv_07.py
@@ -1,11 +1,6 @@
-'''Crie uma função que gere um número aleatório entre 1 e 6, simulando o
-lançamento de um dado.'''
-
 import random
-
 def dado(min, max):
     return random.randint(min, max)
 
-n = dado(1, 6)
-
-print(f"O dado girou e um {n} apareceu.")
+resul = dado(1, 6)
+print(f"Resultado: {resul}")
