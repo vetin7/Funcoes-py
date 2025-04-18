0d4e9fc6da108c8f02eb4700daf48f9de2e548fd
vetin7 <overgames32661080@gmail.com>
2025-04-17 21:54:04 -0300

    Update atv_02.py

--- atv_02.py
+++ atv_02.py
@@ -1,35 +1,27 @@
-'''Crie funções para converter quilômetros em milhas, metros em
-centímetros e litros em mililitros.'''
+def milhas(num1):
+    milhas = num1 * 0.621371
+    return milhas
 
-def milhas(n1):
-    milha = n1 * 0.621371
-    return milha
-
-def centimetros(n1):
-    cent = n1 * 100
+def centimetros(num1):
+    cent = num1 * 100
     return cent
 
-def mililitros(n1):
-    ml = n1 * 1000
+def mililitros(num1):
+    ml = num1 * 1000
     return ml
 
-n1 = int(input("Diga o valor a ser convertido: "))
+num1 = int(input("Diga o valor a ser convertido: "))
 conv = input("Diga para o que ele sera convertido: ")
 
-m = milhas(n1)
-
-c = centimetros(n1)
-
-ml = mililitros(n1)
+m = milhas(num1)
+c = centimetros(num1)
+ml = mililitros(num1)
 
 if conv == "milhas":
-    print(f"O valor {n1} em {conv} é igual a {m}")
-
+    print(f"O valor {num1} em {conv} é igual a {m}")
 elif conv == "centimetros":
-    print(f"O valor {n1} em {conv} é igual a {c}")
-
+    print(f"O valor {num1} em {conv} é igual a {c}")
 elif conv == "mililitros":
     print(f"O valor {n1} em {conv} é igual a {ml}")
-
 else:
-    print("Comando errado :(")
+    print("Comando errado.")
